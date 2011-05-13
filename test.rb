2c460dcd3411756f50662a0515ffa3205911182b
Pieter de Bie <pdebie@ai.rug.nl>
2011-05-13 21:16:49 +0200

    Lay out the battle plan

---
+++ test.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+PREFIX = "2001:470:1f15:1372"
+INTERFACE = "en1"
+GAME_ID = "1234"
+
+# Remove any old IPv6 addresses
+# Remove any old firewire rules
+
+# Add all necessary IPv6 addresses
+# Generate a random grid with TRUE / FALSE
+# Add FW rules for the FALSE ones
+# Add default rules for the TRUE ones?
